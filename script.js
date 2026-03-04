e4288478e65c3aaf55522ba8fb5552780ed0b57c
Brandon <235469637+brandon-04@users.noreply.github.com>
2026-03-04 21:34:55 +0000

    Added home screen.
    Added home screen and infrastructure for future pages.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 localStorage.setItem("pin", "1234");
+localStorage.setItem("balance", "1000");
 let attempts = 0;
+let locationDisplay = document.querySelector("#location");
+let buttonNo = document.querySelector("#buttonNo");
+let buttonYes = document.querySelector("#buttonYes");
 
 setTime();
 setInterval(setTime, 1000);
-function setTime(){
+
+
+function setTime() {
     let timeDisplay = document.querySelector("#time");
     let dateDisplay = document.querySelector("#date");
 
@@ -23,59 +29,124 @@ function setTime(){
 
     timeDisplay.innerHTML = fullTime;
     dateDisplay.innerHTML = fullDate;
-}    
-
+}
 function formatDate(number) {
     return number < 10 ? `0${number}` : number;
 }
 
 pinEntry();
-
 function pinEntry() {
-    let buttonNo = document.querySelector("#buttonNo");
-    let buttonYes = document.querySelector("#buttonYes");
+    locationDisplay.textContent = "Pin Entry";
 
     let pinDisplay = document.querySelector("#pinEntryTextDisplay");
     let typedPin = "";
-    
-    for(let i = 0; i < 10; i++) {
+
+    clearPinEntry();
+
+    for (let i = 0; i < 10; i++) {
         let button = document.querySelector(`#button${i}`);
 
         button.addEventListener("click", () => {
-            if(typedPin.length !=4){
+            if (typedPin.length != 4) {
                 typedPin += i;
                 pinDisplay.textContent += "*";
             }
         });
     }
 
-    buttonNo.addEventListener("click", () => {
-        typedPin = "";
-        pinDisplay.textContent = "";
-    });  
-
+    buttonNo.addEventListener("click", clearPinEntry);
     buttonYes.addEventListener("click", () => {
         checkPin(typedPin);
-    })  
-}
+    })
 
-
-function checkPin(num) {
-    
-    if(num == localStorage.getItem("pin")){
-        alert("correct pin!");
-        //move onto next page
-    }
-    else {
-        attempts += 1;
-        alert(`Incorrect PIN. ${3-attempts} left.`);
-
+    //pin entry exclusive functions
+    function clearPinEntry() {
         typedPin = "";
         pinDisplay.textContent = "";
-        
-        if(attempts == 3) {
-            alert("No more tries.")
-            
+    }
+
+    function checkPin(num) {
+        if (num == localStorage.getItem("pin")) {
+            togglePinEntry();
+            toggleHomeScreen();
+            buttonNo.removeEventListener("click", clearPinEntry);
+        }
+        else {
+            attempts += 1;
+            alert(`Incorrect PIN. ${3 - attempts} left.`);
+
+            if (attempts == 3) {
+                togglePinEntry();
+                toggleLockOutScreen();
+            }
         }
     }
 }
+function homeScreen() {
+    let buttonBalance = document.querySelector("#balanceButton");
+    let buttonWithdraw = document.querySelector("#withdrawButton"); 
+    let buttonPinChange = document.querySelector("#pinChangeButton"); 
+
+    buttonNo.addEventListener("click", () => {
+        toggleHomeScreen();
+        togglePinEntry();
+    });
+
+    balanceButton.addEventListener("click", () => {
+        toggleHomeScreen();
+        //balance page
+    });
+    withdrawButton.addEventListener("click", () => {
+        toggleHomeScreen();
+        //withdraw page
+    });
+    pinChangeButton.addEventListener("click", () => {
+        toggleHomeScreen();
+        //pin change screen
+    });
+
+    
+}
+
+function toggleBalanceScreen() {
+
+}
+
+function toggleLockOutScreen() {
+    let objects = document.getElementsByClassName("lockout");
+
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].classList.toggle("hidden");
+    }
+}
+function togglePinEntry() {
+    let objects = document.getElementsByClassName("pinEntry");
+
+    pinEntry();
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].classList.toggle("hidden");
+    }
+}
+function toggleHomeScreen() {
+    locationDisplay.textContent = "Home";
+
+    let objects = document.getElementsByClassName("home");
+
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].classList.toggle("hidden");
+    }
+
+    homeScreen();
+}
+
+
+
+
+
+
+
+
+//remaining screens and issues
+//1 - lock out screen for when there is too many tries
+//2 - need a home page, withdrawal, balance and change pin
+//3 - hiding screens
